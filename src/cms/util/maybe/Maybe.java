1d04c5c006b2ec1b89e42caf57cf6330c38f0eb2
Andrew Myers <andru@cs.cornell.edu>
2024-12-04 15:19:40 -0500

    fix spec

--- src/cms/util/maybe/Maybe.java
+++ src/cms/util/maybe/Maybe.java
@@ -92,7 +92,7 @@ public abstract class Maybe<T> implements Set<T> {
      */
     public abstract T orElseGet(Supplier<? extends T> other);
 
-    /** Returns the contained value, if any; otherwise, throws the specified exception. */
+    /** Returns the contained value, if any; otherwise, throws the supplied exception. */
     public abstract <E extends Throwable> T orElseThrow(Supplier<E> throwable) throws E;
     
     /** Returns this if a value is contained; otherwise, returns {@code other.get()}.
